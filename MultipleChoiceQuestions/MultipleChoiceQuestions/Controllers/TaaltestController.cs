4429b8c83a76210df9c37592685cac7b53bb1ddd
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-05-26 18:34:40 +0200

    First commit
    Added actionlink to test page

---
+++ MultipleChoiceQuestions/MultipleChoiceQuestions/Controllers/TaaltestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultipleChoiceQuestions.Controllers
+{
+    public class TaaltestController : Controller
+    {
+        //
+        // GET: /Taaltest/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MultipleChoiceQuestions/MultipleChoiceQuestions/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MultipleChoiceQuestions.MvcApplication" Language="C#" %>
