e023774dc53088d330aa6e8a11b9fd69414bca89
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-06-01 14:38:59 +0200

    Added DAL Test project

---
+++ BeterVervoegen/BeterVervoegen.Test/TestBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace BeterVervoegen.Test
+{
+    public class TestBase
+    {
+        public void Describes(string message)
+        {
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine(message);
+            Console.WriteLine("--------------------------------");
+
+        }
+        public void IsPending()
+        {
+            Console.WriteLine(" {0} -- PENDING -- ", GetCaller());
+            Assert.Inconclusive();
+        }
+        public string GetCaller()
+        {
+            StackTrace stack = new StackTrace();
+            return stack.GetFrame(2).GetMethod().Name.Replace("_", " ");
+        }
+    }
+}
