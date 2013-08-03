ae5bd6b4f26df7e1d158bab739952921786c8672
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-08-03 21:57:51 +0200

    Sample prj to test Stateserver session mgmt

---
+++ ServerStateLoadBalancingTest/ServerStateLoadBalancingTest/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServerStateLoadBalancingTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Welcome/
+
+        public ActionResult Index()
+        {
+
+            if (Session["Test"] == null)
+            {
+                Session["Test"] = Guid.NewGuid().ToString();
+            }
+            ViewBag.MyToken = Session["Test"];
+            return View();
+        }
+
+    }
+}
