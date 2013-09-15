06e9fd7abe36d33262ba4fd9f03702f131b3728b
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-09-15 19:02:54 +0200

    'Merged' vs2012 project

---
+++ BeterVervoegen/BeterVervoegen.Specflow/TestEvaluationSteps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace BeterVervoegen.Specflow
+{
+	[Binding]
+	public class TestEvaluationSteps
+	{
+		
+		[Given(@"the infinitive is '(.*)'")]
+		public void GivenTheInfinitiveIs(string p0)
+		{
+			var question = new TestItem(infinitive: "zijn", simplePast: "was", pastParticiple: "geweest");
+
+			ScenarioContext.Current.Add(testItemKey, question);
+		}
+
+		[Given(@"I have entered '(.*)' as simple past")]
+		public void GivenIHaveEnteredAsSimplePast(string p0)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[Given(@"I have entered '(.*)' as past participle")]
+		public void GivenIHaveEnteredAsPastParticiple(string p0)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[When(@"I press submit")]
+		public void WhenIPressSubmit()
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[Then(@"the result should be '(.*)' on the screen")]
+		public void ThenTheResultShouldBeOnTheScreen(string p0)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		public string testItemKey="Question";
+	}
+}
