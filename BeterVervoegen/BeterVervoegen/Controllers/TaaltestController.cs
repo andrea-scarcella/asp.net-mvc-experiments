959bce581a953f9836b8f47223ce28f226276114
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-09-16 23:29:03 +0200

    Added Automapper, fixed null infinitive bug

--- BeterVervoegen/BeterVervoegen/Controllers/TaaltestController.cs
+++ BeterVervoegen/BeterVervoegen/Controllers/TaaltestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
 using BeterVervoegen.BL;
 //using NSubstitute;
 using BeterVervoegen.DAL;
@@ -18,21 +19,35 @@ namespace BeterVervoegen.Controllers
 		public ActionResult Index()
 		{
 
+			Test t0 = new Test(
+				new[] {
+					new TestItem{
+						ItemId=1,
+						Infinitive="zijn",
+						SimplePast="was",
+						PastParticiple="geweest"},new TestItem{
+						ItemId=2,
+						Infinitive="lopen",
+						SimplePast="liep",
+						PastParticiple="gelopen"}});
 
-			TestVM t = new TestVM(new[] {
-				new TestItemVM { 
-					ItemId = 1, 
-					Infinitive = "zijn", 
-					
-					AnswerPastParticiple = "", AnswerSimplePast = "" },
-					new TestItemVM { 
-					ItemId = 2, 
-					Infinitive = "lopen", 
-					 
-					AnswerPastParticiple = "", AnswerSimplePast = "" }
-			}
-			);
-			return View(t);
+			TestVM dto = Mapper.Map<TestVM>(t0);
+
+
+			//TestVM t = new TestVM(new[] {
+			//	new TestItemVM { 
+			//		ItemId = 1, 
+			//		Infinitive = "zijn", 
+
+			//		AnswerPastParticiple = "", AnswerSimplePast = "" },
+			//		new TestItemVM { 
+			//		ItemId = 2, 
+			//		Infinitive = "lopen", 
+
+			//		AnswerPastParticiple = "", AnswerSimplePast = "" }
+			//}
+			//);
+			return View(dto);
 		}
 		[HttpPost]
 		public ActionResult Index(TestVM taalTest)
