0e20389b72f9c1dd5ca77420c2d69db09e7243fe
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-10-24 21:20:37 +0200

    Added items missing from previous commits

---
+++ BeterVervoegen/BeterVervoegen/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BeterVervoegen.Models;
+
+namespace BeterVervoegen.Controllers
+{
+	public class UserController : Controller
+	{
+		[ChildActionOnly]
+		public PartialViewResult UserInfo()
+		{
+			//  var userInfo = System.Threading.Thread.CurrentPrincipal.Identity.
+			var u = HttpContext.User;
+			// .. get the user information from session or db
+			LoginVm l0 = null;
+			if (u.Identity.IsAuthenticated)
+			{
+				l0 = new LoginVm();
+				l0.Username = u.Identity.Name; 
+			}
+			return PartialView(l0);
+		}
+
+	}
+}
