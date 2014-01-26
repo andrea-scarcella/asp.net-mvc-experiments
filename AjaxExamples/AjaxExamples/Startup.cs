caf843a10be8aad2fba9b623d8369f46f13d43e0
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2014-01-26 02:55:59 +0100

    Got simple ajax call to work
    (added link to jquery.unobtrusive-ajax.js to _Layout.cshtml)

---
+++ AjaxExamples/AjaxExamples/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AjaxExamples.Startup))]
+namespace AjaxExamples
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
