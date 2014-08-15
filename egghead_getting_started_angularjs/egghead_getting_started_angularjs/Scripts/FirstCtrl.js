3b0757c9d43ef2913dccd7372617ab1fd595998c
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2014-08-15 18:53:07 +0200

    Added first controller
    Fixed thanks to
    
    http://stackoverflow.com/questions/25330193/angularjs-asp-net-mvc-controller-undefined/25330617#25330617

---
+++ egghead_getting_started_angularjs/egghead_getting_started_angularjs/Scripts/FirstCtrl.js
@@ -0,0 +1,5 @@
+﻿function FirstCtrl($scope) {
+    $scope.data = { message: "Cat!" };
+};
+//required by AngularJs 1.3, first argument of module is app name, also used in ng-app="app name"
+angular.module('gettingStartedApp', []).controller('FirstCtrl', FirstCtrl);
